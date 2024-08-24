a30890ccd6a6a32cfb0cdd1ba04f3529126d5230
richardnv <richardnv@gmail.com>
2024-08-24 19:58:28 -0400

    Mongodb and API

--- INIT_AFTER_CLONE.md
+++ INIT_AFTER_CLONE.md
@@ -1,16 +1,37 @@
-## Navigate to the {Repo}{server} directory ##
+** set up and initialize the dev environment **
+'''
 cd /home/project/IBM-FS-Dev-Capstone/server
+
 pip install virtualenv
+
 virtualenv djangoenv
+
 source djangoenv/bin/activate
+
 python3 -m pip install -U -r requirements.txt
+
 python3 manage.py makemigrations
+
 python3 manage.py migrate
 
+'''
 
-## After that completes ##
-## Navigate to the {Repo}{server}{frontend} directory ##
+** stasic pages and front end and user auth and mgmt **
+'''
 cd /home/project/IBM-FS-Dev-Capstone/server/frontend
+
 npm install
+
 npm run build
 
+'''
+
+** set up and initialize the mongodb database and nodejs API **
+'''
+cd /home/project/IBM-FS-Dev-Capstone/server/database
+
+docker build . -t nodeapp
+
+docker-compose up
+
+'''
--- server/database/app.js
+++ server/database/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
+const cors = require('cors')
 const app = express()
 const port = 3030;
 
@@ -69,7 +69,7 @@ app.get('/fetchDealers', async (req, res) => {
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
     try {
-        const documents = await Reviews.find({dealership: req.params.state});
+        const documents = await Dealerships.find({state: req.params.state});
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -79,7 +79,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-        const documents = await Reviews.find({dealership: req.params.id});
+        const documents = await Dealerships.find({id: req.params.id});
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
