b7ef0a8cc47719b85217a1884e1d6322de703e70
Richard Martin <richardnv@gmail.com>
2024-08-25 11:45:55 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,8 +27,8 @@ class CarModel(models.Model):
     year = models.IntegerField(default=2023,
                                validators=[MaxValueValidator(2023),
                                            MinValueValidator(2015)
-                                          ]
-                              )
+                                           ]
+                               )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
